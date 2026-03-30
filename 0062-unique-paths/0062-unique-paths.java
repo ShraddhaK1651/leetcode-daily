0029694fe6258fa4d195d9b9c614f8706b026179
ShraddhaK1651 <shraddhakolli334@gmail.com>
2026-03-30 12:44:33 +0530

    Time: 1 ms (13.75%), Space: 42.3 MB (20.2%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,18 +1,25 @@
+import java.util.Arrays;
+
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp=new int[m][n];
-        for(int[] row:dp){
-        Arrays.fill(row,-1);
+        int[][] dp = new int[m][n];
+
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
         }
-        int ans=res(dp,m-1,n-1);
-        return ans;
+
+        return res(dp, m-1, n-1);
     }
-    public int res(int[][] dp,int i,int j){
-        if(i==0 && j==0)return 1;
-        if(i<0 || j<0)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-         int left=res(dp,i,j-1);
-         int up=res(dp,i-1,j);
-         return dp[i][j]=left+up;
+
+    public int res(int[][] dp, int i, int j){
+        if(i == 0 && j == 0) return 1;
+        if(i < 0 || j < 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int left = res(dp, i, j-1);
+        int up = res(dp, i-1, j);
+
+        return dp[i][j] = left + up;
     }
 }
