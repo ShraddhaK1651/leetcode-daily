c3756490e94a527f3a43d65dd8c952108b5831ef
ShraddhaK1651 <shraddhakolli334@gmail.com>
2026-07-17 12:14:49 +0530

    Time: 7 ms (38.92%), Space: 45.4 MB (29.7%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,21 +1,31 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>>ans=new ArrayList<>();
-        Arrays.sort(candidates);
-        backtrack(0,candidates,target,new ArrayList<>(),ans);
+        List<List<Integer>> ans=new ArrayList<>();
+        combSum(0,candidates,new ArrayList<>(),target,ans);
         return ans;
     }
-    private void backtrack(int index,int[] candidates,int target,List<Integer>ds,List<List<Integer>> ans){
+
+    void combSum(int i,int[] candidates,List<Integer> list,int target,List<List<Integer>> ans){
         if(target==0){
-            ans.add(new ArrayList<>(ds));
-            return;
+        ans.add(new ArrayList<>(list));
+        return;
         }
-        for(int i=index;i<candidates.length;i++){
-            if(i>index && candidates[i]==candidates[i-1])continue; //skip duplicates
-            if(candidates[i]>target)break;
-            ds.add(candidates[i]);
-            backtrack(i+1,candidates,target-candidates[i],ds,ans);
-            ds.remove(ds.size()-1);
-        }
+        if(i==candidates.length)return;
+
+        Arrays.sort(candidates);
+
+        for (int j = i; j < candidates.length; j++) {
+
+        if (j > i && candidates[j] == candidates[j - 1])
+            continue;
+
+        if (candidates[j] > target)
+        break;
+
+        list.add(candidates[j]);
+        combSum(j + 1, candidates, list, target - candidates[j], ans);
+        list.remove(list.size() - 1);
+}
+
     }
 }
